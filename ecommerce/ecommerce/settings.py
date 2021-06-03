49473f5e1a638483e014785b9ab0c4103a16860d
Artur Rejment <arturrejment1@gmail.com>
2021-06-03 19:49:03 +0200

    Connecting Django with Postgres

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 from pathlib import Path
 import os
+from keys import keys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k^x-_thy2p@8e8fs9%g_&nxlsh57^7hyyffmvv=3e^$nk%f+th'
+SECRET_KEY = keys.SEC_DJANGO_KEY
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +81,19 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'ecommerce_app',
+
+        'USER': 'postgres',
+
+        'PASSWORD': keys.DB_PASS,
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField( max_length=120, null=True)
+    email = models.CharField( max_length=120, null=True)
+    
+    def __str__(self):
+        return self.name 
+    
+class Product(models.Model):
+    name = models.CharField(max_length=120, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    # TODO: image field
+    
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=False)
+    date_ordered = models.DateTimeField(auto_now=False, auto_now_add=False)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=50, null=True)
+    
+    def __str__(self):
+        return str(self.id)
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default = 0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now=False, auto_now_add=False)
+    
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=False)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=100, null=False)
+    city = models.CharField(max_length=100, null=False)
+    state = models.CharField(max_length=100, null=False)
+    zipcode = models.CharField(max_length=100, null=False)
+    date_added = models.DateTimeField(auto_now_add = True)
+    
+    def __str__(self):
+        return self.address
+    
