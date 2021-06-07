95fb8ab40e1596db43be88675f4d1946b9486622
Artur Rejment <arturrejment1@gmail.com>
2021-06-07 22:41:37 +0200

    Updating the guest cart

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -33,9 +33,41 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:    
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+        
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
         cartItems = order["get_cart_items"]
+
+        for item in cart:
+            try:
+                cartItems += cart[item]['quantity']
+                
+                product = Product.objects.get(id = item)
+                total = (product.price * cart[item]['quantity'])
+
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[item]['quantity']
+
+                cart_item = {
+                    'product': {
+                        'id': product.id,
+                        'name': product.name,
+                        'price': product.price,
+                        'imageURL': product.imageURL,
+                    },
+                    'quantity': cart[item]['quantity'],
+                    'get_total': total
+                }
+                items.append(cart_item)
+                
+                if product.digital == False:
+                    order['shipping'] = True
+            except:
+                pass
         
     context = {'items':items, 'order':order, "cartItems": cartItems}
     return render(request, 'store/cart.html', context)
